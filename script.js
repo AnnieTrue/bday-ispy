c26856cb427f6a48dbb3ef1455c6e61081841be4
Annie Courtney <atcbuttercup@yahoo.com>
2025-03-12 01:24:20 -0400

    name submit

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+console.log("SCRIPT")
+const form = document.getElementById('nameForm');
+const nameDisplay = document.getElementById('player');
+const pointsDisplay = document.getElementById('points');
+var points = 0
+var name = ""
 if (!localStorage["name"]){
 	document.getElementById("NameScreen").style.display = "flex"
 }else{
 	name = localStorage["name"]
-	document.getElementById("GameScreen").style.display = "flex"
-	document.getElementById("GameScreen").style.display = "none"
+	console.log("Name:" + name)
+	startGameScreen()
 }
+
+function startGameScreen(){
+	console.log("Name:" + name)
+	document.getElementById("GameScreen").style.display = "flex"
+	document.getElementById("NameScreen").style.display = "none"
+
+	if (localStorage["points"]){
+		points = localStorage["points"]
+	}
+
+	nameDisplay.textContent = name
+	pointsDisplay.textContent = points
+}
+
+
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault(); // Prevent default form submission
+	alert("test")
+	console.log("FUNCTION")
+	name = document.getElementById("name").value
+	localStorage["name"] = name
+	startGameScreen()
+});
